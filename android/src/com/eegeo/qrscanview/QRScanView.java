83d0eeff62f6b30ff8f9e0a15cf4023fd28b4b09
Najhi Ullah <najhi_1989@hotmail.com>
2019-01-31 17:40:38 +0500

    Update QR code scan code. Buddy: Qaiser

--- android/src/com/eegeo/qrscanview/QRScanView.java
+++ android/src/com/eegeo/qrscanview/QRScanView.java
@@ -2,25 +2,38 @@
 
 package com.eegeo.qrscanview;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 
 import com.eegeo.entrypointinfrastructure.MainActivity;
+import com.eegeo.helpers.IRuntimePermissionResultHandler;
 import com.eegeo.mobileexampleapp.R;
+import com.google.zxing.Result;
 
 import java.util.List;
 
-public class QRScanView implements View.OnClickListener
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class QRScanView implements View.OnClickListener, ZXingScannerView.ResultHandler, IRuntimePermissionResultHandler
 {
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1000;
     protected MainActivity m_activity = null;
     protected long m_nativeCallerPointer;
     private View m_view = null;
     private RelativeLayout m_uiRoot = null;
     private View m_closeButton = null;
+    private RelativeLayout m_qrScanContainerView;
+    private ZXingScannerView m_scannerView;
 
     public QRScanView(MainActivity activity, long nativeCallerPointer)
     {
@@ -31,6 +44,8 @@ public class QRScanView implements View.OnClickListener
 
         m_view = m_activity.getLayoutInflater().inflate(R.layout.qr_scan_layout, m_uiRoot, false);
         m_closeButton = m_view.findViewById(R.id.qr_scan_view_close_button);
+
+        m_qrScanContainerView = m_view.findViewById(R.id.qr_scan_container_view);
 
         RelativeLayout.LayoutParams layoutParams = (LayoutParams) m_view.getLayoutParams();
         if (m_activity.getResources().getBoolean(R.bool.isPhone)) 
@@ -44,18 +59,12 @@ public class QRScanView implements View.OnClickListener
         }
 
         m_closeButton.setOnClickListener(this);
-        View clickMeButton = m_view.findViewById(R.id.qr_scan_click_me);
-        clickMeButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                onQRScanCompleted("wrld://fixedlocation/indoor/31.496739/74.421984/EIM-908710f5-3ed3-408d-a92b-c7749d9f1ae1/0");
-            }
-        });
+//        onQRScanCompleted("wrld://fixedlocation/indoor/31.496739/74.421984/EIM-908710f5-3ed3-408d-a92b-c7749d9f1ae1/0");
 
         m_view.setVisibility(View.GONE);
         m_uiRoot.addView(m_view);
+
+        m_activity.getRuntimePermissionDispatcher().addRuntimePermissionResultHandler(this);
 
     }
 
@@ -65,14 +74,14 @@ public class QRScanView implements View.OnClickListener
         String host = url.getHost();
         List<String> pathSegments = url.getPathSegments();
 
-        if (host.equals("fixedlocation"))
+        if (host!=null && host.equals("fixedlocation"))
         {
-            if (pathSegments.size() == 5)
+            if (pathSegments!=null && pathSegments.size() == 5)
             {
                 QRScanViewJniMethods.OnQRScan(m_nativeCallerPointer, host + pathSegments.get(0), Double.parseDouble(pathSegments.get(1)), Double.parseDouble(pathSegments.get(2)), pathSegments.get(3), Double.parseDouble(pathSegments.get(4)));
                 QRScanViewJniMethods.CloseButtonClicked(m_nativeCallerPointer);
             }
-            else if (pathSegments.size() == 4)
+            else if (pathSegments!=null && pathSegments.size() == 4)
             {
                 QRScanViewJniMethods.OnQRScan(m_nativeCallerPointer, host, Double.parseDouble(pathSegments.get(0)), Double.parseDouble(pathSegments.get(1)), pathSegments.get(2), Double.parseDouble(pathSegments.get(3)));
                 QRScanViewJniMethods.CloseButtonClicked(m_nativeCallerPointer);
@@ -109,6 +118,7 @@ public class QRScanView implements View.OnClickListener
 
     public void destroy()
     {
+        m_activity.getRuntimePermissionDispatcher().removeIRuntimePermissionResultHandler(this);
         m_uiRoot.removeView(m_view);
     }
 
@@ -127,11 +137,34 @@ public class QRScanView implements View.OnClickListener
         m_closeButton.setEnabled(true);
         m_view.setVisibility(View.VISIBLE);
         m_view.requestFocus();
+
+        if(m_activity.getRuntimePermissionDispatcher().hasCameraPermissionsWithCode(CAMERA_PERMISSION_REQUEST_CODE))
+        {
+            startQRScanCamera();
+        }
     }
 
     public void dismissQRScan()
     {
+        stopQRScanCamera();
         m_view.setVisibility(View.GONE);
+    }
+
+    private void startQRScanCamera()
+    {
+        m_scannerView = new ZXingScannerView(m_activity);
+        m_qrScanContainerView.addView(m_scannerView);
+        m_scannerView.setResultHandler(this);
+        m_scannerView.startCamera();
+    }
+
+    private void stopQRScanCamera()
+    {
+        if(m_scannerView!=null)
+        {
+            m_scannerView.stopCamera();
+        }
+        m_qrScanContainerView.removeAllViews();
     }
 
     public void onClick(View view)
@@ -139,4 +172,86 @@ public class QRScanView implements View.OnClickListener
         m_closeButton.setEnabled(false);
         QRScanViewJniMethods.CloseButtonClicked(m_nativeCallerPointer);
     }
+
+    @Override
+    public void handleResult(Result result)
+    {
+        onQRScanCompleted(result.getText());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        if(requestCode != CAMERA_PERMISSION_REQUEST_CODE)
+            return;
+
+        // If request is cancelled, the result arrays are empty.
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(takePictureIntent.resolveActivity(m_activity.getPackageManager()) != null)
+            {
+                startQRScanCamera();
+            }
+            else
+            {
+                showCameraErrorDialog();
+            }
+        }
+        else
+        {
+            // If any of the permission is denied, we can't use the camera
+            // properly, so we will show the dialog with agree or cancel dialog
+            showPermissionRequiredDialog(m_activity);
+        }
+        return;
+
+    }
+
+    private void showCameraErrorDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(m_activity);
+        builder.setTitle("Error");
+        builder.setMessage("Error opening camera. Make sure device camera is not 'disabled'");
+        builder.setNegativeButton("Ok", null);
+        builder.setCancelable(false);
+        builder.show();
+    }
+
+    private void showPermissionRequiredDialog(final Activity context)
+    {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.dismiss();
+                switch(which)
+                {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(context, Manifest.permission.CAMERA))
+                        {
+                            // Checking Permissions again
+                            m_activity.getRuntimePermissionDispatcher().hasCameraPermissionsWithCode(CAMERA_PERMISSION_REQUEST_CODE);
+                        }
+                        else
+                        {
+                            // Open App Settings Page
+                            m_activity.getRuntimePermissionDispatcher().startAppSettings(context);
+                        }
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        QRScanViewJniMethods.CloseButtonClicked(m_nativeCallerPointer);
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage(context.getResources().getString(R.string.required_camera_permission_text))
+                .setPositiveButton(context.getResources().getString(R.string.ok_text), dialogClickListener)
+                .setNegativeButton(context.getResources().getString(R.string.cancel_text), dialogClickListener).show();
+    }
+
 }
