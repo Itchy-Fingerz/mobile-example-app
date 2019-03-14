167f99507dd29420b8ea63d21e050056edc5388d
sohail khan <sohail.khan@netsoltech.com>
2019-03-14 14:30:09 +0500

    EAG-67 Zoom levels and start points for the ship interior refinements buddy:salman

--- src/InteractionModelStateChangedObserver/InteractionModelStateChangedObserver.cpp
+++ src/InteractionModelStateChangedObserver/InteractionModelStateChangedObserver.cpp
@@ -63,47 +63,49 @@ namespace ExampleApp
             
             Eegeo::Space::LatLong locFloor1 = Eegeo::Space::LatLong::FromDegrees(26.15516487293898, -78.79296869686519);
             Eegeo::dv3 interest1 = locFloor1.ToECEF();
-            heading = Eegeo::Math::Deg2Rad(72.4);
+            heading = Eegeo::Math::Deg2Rad(78.4);
             m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(1,interest1));
-            m_floorsZoomLevel.insert(std::pair<int, float>(1,350.2683410644531));
+            m_floorsZoomLevel.insert(std::pair<int, float>(1,250.2683410644531));
             m_floorsHeading.insert(std::pair<int, float>(1,heading));
             
             
             const Eegeo::Space::LatLong locFloor2 = Eegeo::Space::LatLong::FromDegrees(26.15516638853986, -78.79241370024458);
             const Eegeo::dv3 interest2 = locFloor2.ToECEF();
-            heading = Eegeo::Math::Deg2Rad(72.4);
+            heading = Eegeo::Math::Deg2Rad(75.4);
 
             m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(2,interest2));
-            m_floorsZoomLevel.insert(std::pair<int, float>(2,300));
+            m_floorsZoomLevel.insert(std::pair<int, float>(2,270));
             m_floorsHeading.insert(std::pair<int, float>(2,heading));
             
+            heading = Eegeo::Math::Deg2Rad(75.4);
             m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(3,interest2));
-            m_floorsZoomLevel.insert(std::pair<int, float>(3,250));
+            m_floorsZoomLevel.insert(std::pair<int, float>(3,240));
             m_floorsHeading.insert(std::pair<int, float>(3,heading));
             
             const Eegeo::dv3 interest4 = Eegeo::Space::LatLong::FromDegrees(26.155162289253884, -78.79261573860019).ToECEF();
             m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(4,interest4));
-            m_floorsZoomLevel.insert(std::pair<int, float>(4,250));
+            m_floorsZoomLevel.insert(std::pair<int, float>(4,270));
             m_floorsHeading.insert(std::pair<int, float>(4,heading));
             
-            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(5,interestPoint));
-            m_floorsZoomLevel.insert(std::pair<int, float>(5,250));
+            const Eegeo::dv3 interest5 = Eegeo::Space::LatLong::FromDegrees(26.15516376558429, -78.79266780055974).ToECEF();
+            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(5,interest5));
+            m_floorsZoomLevel.insert(std::pair<int, float>(5,230));
             m_floorsHeading.insert(std::pair<int, float>(5,heading));
             
-            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(6,interestPoint));
-            m_floorsZoomLevel.insert(std::pair<int, float>(6,250));
+            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(6,interest5));
+            m_floorsZoomLevel.insert(std::pair<int, float>(6,230));
             m_floorsHeading.insert(std::pair<int, float>(6,heading));
             
-            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(7,interestPoint));
-            m_floorsZoomLevel.insert(std::pair<int, float>(7,250));
+            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(7,interest5));
+            m_floorsZoomLevel.insert(std::pair<int, float>(7,230));
             m_floorsHeading.insert(std::pair<int, float>(7,heading));
             
-            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(8,interestPoint));
-            m_floorsZoomLevel.insert(std::pair<int, float>(8,250));
+            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(8,interest5));
+            m_floorsZoomLevel.insert(std::pair<int, float>(8,230));
             m_floorsHeading.insert(std::pair<int, float>(8,heading));
             
-            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(9,interestPoint));
-            m_floorsZoomLevel.insert(std::pair<int, float>(9,250));
+            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(9,interest5));
+            m_floorsZoomLevel.insert(std::pair<int, float>(9,230));
             m_floorsHeading.insert(std::pair<int, float>(9,heading));
             
             const Eegeo::dv3 interestPoint10 = Eegeo::Space::LatLong::FromDegrees(26.15514838609565, -78.79279665517699).ToECEF();
@@ -111,17 +113,18 @@ namespace ExampleApp
             m_floorsZoomLevel.insert(std::pair<int, float>(10,250));
             m_floorsHeading.insert(std::pair<int, float>(10,heading));
 
-            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(11,interestPoint10));
-            m_floorsZoomLevel.insert(std::pair<int, float>(11,300));
+            const Eegeo::dv3 interestPoint12 = Eegeo::Space::LatLong::FromDegrees(26.155147247944303, -78.79300009889178).ToECEF();
+            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(11,interestPoint12));
+            m_floorsZoomLevel.insert(std::pair<int, float>(11,220));
             m_floorsHeading.insert(std::pair<int, float>(11,heading));
             
-            heading = Eegeo::Math::Deg2Rad(75.4);
-            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(12,interestPoint10));
-            m_floorsZoomLevel.insert(std::pair<int, float>(12,300));
+            const Eegeo::dv3 interestPoint13 = Eegeo::Space::LatLong::FromDegrees(26.155135715755936, -78.79304823530377).ToECEF();
+            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(12,interestPoint13));
+            m_floorsZoomLevel.insert(std::pair<int, float>(12,210));
             m_floorsHeading.insert(std::pair<int, float>(12,heading));
             
-            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(13,interestPoint10));
-            m_floorsZoomLevel.insert(std::pair<int, float>(13,300));
+            m_floorsLocation.insert(std::pair<int, Eegeo::dv3>(13,interestPoint13));
+            m_floorsZoomLevel.insert(std::pair<int, float>(13,225));
             m_floorsHeading.insert(std::pair<int, float>(13,heading));
             
         }
