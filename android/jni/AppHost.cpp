9fc6cb218b7f39c7084d26a526d65ad6a9355e12
Ali Arslan <ali.arslan@wrld3d.com>
2019-03-29 12:27:55 +0000

    Removed old debug models. Buddy: Malcolm B

--- android/jni/AppHost.cpp
+++ android/jni/AppHost.cpp
@@ -161,8 +161,6 @@ AppHost::AppHost(
     std::set<std::string> customApplicationAssetDirectories;
     customApplicationAssetDirectories.insert("SearchResultOnMap");
     customApplicationAssetDirectories.insert("ApplicationConfigs");
-    customApplicationAssetDirectories.insert("AnimatedModels/Axis");
-    customApplicationAssetDirectories.insert("AnimatedModels/RobotArm");
     customApplicationAssetDirectories.insert("AnimatedModels/Stormtrooper");
     customApplicationAssetDirectories.insert("AnimatedModels/Duck");
     customApplicationAssetDirectories.insert("AnimatedModels/Xwing");
