8634f6846539ac4dc72dd29bea11dd43ff87f3a8
Qaiser <malikqaiser.shehzad@gmail.com>
2019-05-31 16:59:10 +0500

    EAG-143 eagle AR app trust cert fixed from eagle App buddy:Salman

--- ios/ios_src/Utils/IosUtils.mm
+++ ios/ios_src/Utils/IosUtils.mm
@@ -25,7 +25,11 @@ namespace ExampleApp
             
             if ([application canOpenURL:URL]) {
                 // Safe to launch the AR App app
-                [application openURL:URL];
+                BOOL success = [application openURL:URL];
+                if (!success)
+                {
+                    IosUtils::notifyAppNotTrusted();
+                }
             }
             else
             {
@@ -36,6 +40,19 @@ namespace ExampleApp
         void IosUtils::notifyAppNotInstalled()
         {
             UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Error" message:@"Eagle AR app is not installed on your device." preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                
+            }];
+            [alert addAction:ok];
+            
+            UIWindow *window = [[UIApplication sharedApplication] keyWindow];
+            UIViewController *viewController = window.rootViewController;
+            [viewController presentViewController:alert animated:YES completion:nil];
+        }
+        
+        void IosUtils::notifyAppNotTrusted()
+        {
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Error" message:@"Trust Eagle AR certificate from device settings" preferredStyle:UIAlertControllerStyleAlert];
             UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
                 
             }];
