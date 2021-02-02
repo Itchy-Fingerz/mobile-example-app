1fbe8caec9ccdbc8120c5ed67100ad999bd6df49
salmankhalid187 <salmankhalid1@gmail.com>
2021-02-02 17:19:41 +0500

    Fixed xcode issue + platform update script issue fixed

--- update.platform.sh
+++ update.platform.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 -p android|ios|windows [-c]"; echo "  -p -> platform, ios, android or windows (required)"; 1>&2; exit 1; }
+usage() { echo "Usage: $0 -p android|ios|windows [-c]"; echo "  -p -> platform, ios, android or windows (required), -v -> {teamcitybuildnumber} (optional)"; 1>&2; exit 1; }
 
 baseUrl="http://s3.amazonaws.com/eegeo-static/"
 srcPackageName="INVALID"
@@ -8,23 +8,28 @@ destPackageName="./sdk.package.tar.gz"
 includeDestination="INVALID"
 sdkDestination="INVALID"
 
-while getopts "p:" o; do
+while getopts "p:v:" o; do
     case "${o}" in
         p)
-            p=${OPTARG}
-            if [ "$p" != "ios" ]; then
-               if [ "$p" != "android" ]; then
-	          if [ "$p" != "windows" ]; then
-                     usage
-		  fi
-               fi
+          p=${OPTARG}
+          if [ "$p" != "ios" ]; then
+            if [ "$p" != "android" ]; then
+              if [ "$p" != "windows" ]; then
+                  usage
+              fi
             fi
-            ;;
+          fi
+        ;;
+        v)
+          v=${OPTARG}
+          echo "sdk version set to v${v}"
+        ;;
         *)
-            usage
-            ;;
+          usage
+        ;;
     esac
 done
+
 shift $((OPTIND-1))
 
 if [ -z "${p}" ]; then
@@ -32,36 +37,39 @@ if [ -z "${p}" ]; then
 fi
 
 if [ "$p" == "ios" ]; then
-   srcPackageName="sdk.package.ios"
+   srcPackageName="sdk.package.ios.cpp11.tar.gz"
    includeDestination="./ios/Include/eegeo"
    sdkDestination="sdk.package"
 elif [ "$p" == "android" ]; then
-   srcPackageName="sdk.package.android"
+   srcPackageName="sdk.package.android.cpp11.c++_static.tar.gz"
    includeDestination="./android/libs/eegeo"
    sdkDestination="sdk.package.android"
 elif [ "$p" == "windows" ]; then
-   srcPackageName="sdk.package.windows"
+   srcPackageName="sdk.package.windows.tar.gz"
    includeDestination="./windows/libs/eegeo"
    sdkDestination="sdk.package.windows"
-
 fi
 
-if [ "$p" == "windows" ]; then
-   srcPackageName="$srcPackageName.tar.gz"
-else
-   srcPackageName="$srcPackageName.cpp11.tar.gz"
+downloadUrl="${baseUrl}wrldsdk/v${v}/${srcPackageName}"
+
+if [ -z "${v}" ]; then
+   echo "using latest version of SDK"
+   downloadUrl="${baseUrl}${srcPackageName}"
 fi
+
+downloadUrl=$(echo ${downloadUrl} | sed "s:+:%2B:g")
 
 echo "Updating $p platform..."
 rm -f ./$destPackageName
 rm -rf $includeDestination
-curl $baseUrl$srcPackageName > ./$destPackageName
+echo "$downloadUrl"
+curl $downloadUrl > ./$destPackageName
 
 statuscode=$?
 if [ $statuscode -ne 0 ] ; then
-    echo "Failed to download sdk package ${baseUrl}${srcPackageName}" >&2
+    echo "Failed to download sdk package ${downloadUrl}" >&2
     exit $statuscode
-fi    
+fi
 
 if [ ! -d `dirname "$includeDestination"` ]; then
     mkdir -p `dirname "$includeDestination"`
@@ -71,7 +79,7 @@ tar -zxvf $destPackageName
 
 if [ $? -ne 0 ] ; then
     echo "Failed to unzip sdk package ${destPackageName}"
-    exit 1    
+    exit 1
 fi
 
 rm -f ./$destPackageName
